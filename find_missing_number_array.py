87af6410343459ee0a3ad15967155c88b0b6691f
Izzat Zainol <izzatz13@gmail.com>
2019-03-28 23:08:58 +0800

    Create find_missing_number_array.py

---
+++ find_missing_number_array.py
@@ -0,0 +1,15 @@
+# Find a missing number in an array
+
+new_array = [1,2,3,5]
+
+total_array = len(new_array)
+print("Total numbers in array is ", total_array)
+
+total = (total_array+1) * (total_array+2)/2 
+print(total)
+
+sum_all_array = sum(new_array)
+print("Total sum of all numbers in the array: ", sum_all_array)
+
+missing_num = total - sum_all_array
+print("The missing number is: ", missing_num)
